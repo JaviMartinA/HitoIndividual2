44aa00e46de5f9d4208afc1a62652a247d937c5f
Javier Martin Arroyo <93380199+JaviMartinA@users.noreply.github.com>
2021-12-03 13:36:36 +0100

    Create Cuestion3_Compra.java

---
+++ Cuestion3_Compra.java
@@ -0,0 +1,12 @@
+package Cuestion3;
+
+public abstract class Compra {
+	String marca;
+	float precio;
+	String supermercado;
+	public void resumen() {
+		System.out.println("El resumen es [marca=" + marca + ", precio=" + precio + ", supermercado=" + supermercado + "]");
+		
+	}
+	
+}
